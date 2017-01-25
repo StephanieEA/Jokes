00ded47edcfd91f4c16832f17adc1c9bb203bf9a
Stephanie Andrews <seandrews11@gmail.com>
2017-01-25 16:45:50 -0700

    switch from hashHistory to browserHistory

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Router, Link, IndexRoute, Route, hashHistory} from 'react-router'
+import {Router, Link, IndexRoute, Route, browserHistory} from 'react-router'
 
 import Home from './components/Home/Home'
 import JokeGrid from './components/JokeGrid/JokeGrid'
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Home}>
       <Route path='/jokes' component={JokeGrid} />
       <IndexRoute component={JokeGrid}/>
