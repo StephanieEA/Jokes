31c3029f71382e9fd66deca63c1e1064a2037226
Stephanie Andrews <seandrews11@gmail.com>
2017-01-25 20:41:39 -0700

    add settings and files for testing

--- app/components/GeneratedJoke/GeneratedJoke.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './joke-grid-style';
-
-const GeneratedJoke = () => {
-  <p>generated joke</p>
-}
-
-export default GeneratedJoke;
---
+++ app/components/Settings/Settings.js
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react'
+//
+// import './configuration-style';
+//
+// export default class Settings extends Component {
+//
+//   render () {
+//     return (
+//       <section>
+//         <h1>YOOOO</h1>
+//       </section>
+//     )
+//   }
+// }
