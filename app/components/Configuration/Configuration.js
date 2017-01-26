62f356685a0d9542aec2271973ff0268482c69a4
Stephanie Andrews <seandrews11@gmail.com>
2017-01-25 20:27:23 -0700

    slight css

---
+++ app/components/Configuration/Configuration.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './configuration-style';
+
+export default class Configuration extends Component {
+
+  render () {
+    return (
+      <section>
+        <h1>YOOOO</h1>
+      </section>
+    )
+  }
+}
