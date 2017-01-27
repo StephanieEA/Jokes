2e9edbd3de42689277c018604a2dc7e56b50eefc
Stephanie Andrews <seandrews11@gmail.com>
2017-01-26 17:40:22 -0700

    favorites is fully hooked up

--- app/components/Controls/Controls.js
+++ app/components/Controls/Controls.js
@@ -17,10 +17,8 @@ export default class Controls extends Component {
           onChange={this.props.handleNumber}
           value={this.props.number}/>
         <Button
-          // will need to alter onClick trigger- right now its just duplicating joke setup
               className='favorites'
               text="Favorites"
-              onClick={this.props.getRandom}
               targetPath='/favorites'
         />
       </div>
--- app/components/Favorites/favorites.js
+++ app/components/Favorites/favorites.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 
+import Button from '../../components/Button/Button'
+
 
 export default class Favorites extends Component {
-
-  renderFavoriteJokes(array) {
-    const jokeArray = array.map((jk, index) =>
-    <p key={index} className='random'>{jk.joke}
-      <span>&#9734;</span>
-    </p>
-    );
-    return jokeArray
-  }
-
   render () {
-    const jokes = this.renderFavoriteJokes(this.props.jokes)
+    let jokes = this.props.jokes.filter(joke => joke.favorite === true)
+
+    jokes = jokes.map((jk, index) =>
+       (<p key={index}
+          id={jk.id}
+          className='random'>
+          {jk.joke}
+         <Button onClick={() => this.props.makeFavorite(jk.id)}
+         className='favorite-btn' text='&#9734;' />
+       </p>)
+      )
+
     return (
       <section>
         {jokes}
